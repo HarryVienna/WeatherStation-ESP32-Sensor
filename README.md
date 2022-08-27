b465d9fd0bf13537aaa77221ed9ee3726e1318b2
Harald Kreuzer <mail@haraldkreuzer.net>
2022-08-27 23:11:46 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ESP32-Weatherstation-Sensor
+# ESP32 SmartConfig Wi-Fi IoT component
+
+ESP32 SmartConfig library that allows you to connect the microcontroller to the Wi-Fi network with just a few lines of code.
+
+
+You can find a detailed description on my [website](https://www.haraldkreuzer.net/en/news/esp32-iot-smartconfig-wifi-provisioning-library).
---
+++ src/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_sleep.h"
+#include "esp_log.h"
+
+#include "wifi.h"
+
+
+RTC_DATA_ATTR static int boot_count = 0;
+
+static const char* TAG = "main";
+ 
+
+void app_main(){
+
+    ++boot_count;
+    ESP_LOGI(TAG, "Boot count: %d", boot_count);
+
+
+    esp_err_t ret;
+
+    static wifi_conf_t wifi_conf = {
+        .aes_key = "ESP32EXAMPLECODE",
+        .hostname = "ESP32",
+        .ntp_server = "pool.ntp.org",
+    };
+    wifi_t *smartconfig = wifi_new_smartconfig(&wifi_conf);
+
+    switch (esp_sleep_get_wakeup_cause()) {
+
+        case ESP_SLEEP_WAKEUP_TIMER: {
+            printf("ESP_SLEEP_WAKEUP_TIMER\n");
+            break;
+        }
+        default: {
+            printf("ESP_SLEEP_WAKEUP_UNDEFINED\n");
+        }
+    }
+
+    if (smartconfig->init(smartconfig) == ESP_OK) {
+
+        do {
+            ret = smartconfig->connect(smartconfig);
+        }
+        while (ret != ESP_OK);
+
+        smartconfig->init_sntp(smartconfig);
+
+        smartconfig->init_timezone(smartconfig);
+    }
+
+    vTaskDelay((1000 * 60) / portTICK_PERIOD_MS); // Wait 1 minute
+
+
+
+    const int deep_sleep_sec = 60 * 10;
+    ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
+    smartconfig->stop(smartconfig);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_MAX, ESP_PD_OPTION_OFF);
+    esp_deep_sleep(1000000LL * deep_sleep_sec);
+}
